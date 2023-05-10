ace4bcff54eaaad7524411fbcb2b7b70827729c0
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-05-10 11:19:21 +0300

    Add config files

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import {fileURLToPath} from 'node:url';
+import {mergeConfig} from 'vite';
+import {configDefaults, defineConfig} from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      setupFiles: ['./vitest.setup.ts'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      deps: {
+        inline: ['vuetify'],
+      },
+    },
+  })
+);
