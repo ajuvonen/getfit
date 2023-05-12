665c541b8e6f731d1553735104fbc16f2baed8bb
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-05-12 09:58:54 +0300

    Add retry to cypress config

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,9 +1,7 @@
 describe('Navigation tests', () => {
   it('navbar navigates to correct routes', () => {
-    cy.visit('/');
-    cy.getByTestId('navbar-schedule-link').click();
-    cy.location('pathname').should('contain', '/schedule');
-    cy.getByTestId('navbar-print-link').click();
+    cy.visit('/').getByTestId('navbar-schedule-link').click();
+    cy.location('pathname').should('contain', '/schedule').getByTestId('navbar-print-link').click();
     cy.contains('h1', 'Print')
       .location('pathname')
       .should('contain', '/print')
