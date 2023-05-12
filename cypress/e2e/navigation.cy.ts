8244de8983ed4da230194bffffac6152dd5bb725
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-05-12 09:54:47 +0300

    Attempt to fix flaky test

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,7 +1,9 @@
 describe('Navigation tests', () => {
   it('navbar navigates to correct routes', () => {
-    cy.visit('/').getByTestId('navbar-schedule-link').click();
-    cy.location('pathname').should('contain', '/schedule').getByTestId('navbar-print-link').click();
+    cy.visit('/');
+    cy.getByTestId('navbar-schedule-link').click();
+    cy.location('pathname').should('contain', '/schedule');
+    cy.getByTestId('navbar-print-link').click();
     cy.contains('h1', 'Print')
       .location('pathname')
       .should('contain', '/print')
