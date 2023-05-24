e0cba24af8d712506fd7eca3ae067682209ddaeb
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-05-24 16:17:34 +0300

    Edit localizations

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {useI18n} from 'vue-i18n';
+
+const {t} = useI18n();
+</script>
 
 <template>
-  <h1 class="text-h2">Home</h1>
+  <h1 class="text-h2 text-center">{{ t('home.title') }}</h1>
 </template>
