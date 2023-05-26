c77767f37aac455fb8000fccb50903543f9a3d91
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-05-26 15:31:54 +0300

    Add print view content

---
+++ src/components/__tests__/SimpleTrainingCard.spec.ts
@@ -0,0 +1,81 @@
+import {mount} from '@vue/test-utils';
+import {describe, it, expect, beforeEach} from 'vitest';
+import {v4 as uuidv4} from 'uuid';
+import SimpleTrainingCard from '@/components/SimpleTrainingCard.vue';
+import {useScheduleStore} from '@/stores/schedule';
+import {useAppStateStore} from '@/stores/appState';
+import {Intensity, type Training} from '@/types';
+
+const basicTraining = {
+  id: uuidv4(),
+  weekId: uuidv4(),
+  dayIndex: 0,
+  activity: 'boxing',
+  title: '',
+  description: 'Sparring at the gym',
+  duration: 1,
+  intensity: Intensity.MEDIUM,
+  completionSummary: '',
+  location: 'Total wreck gym',
+} as Training;
+
+describe('TrainingCard', () => {
+  const scheduleStore = useScheduleStore();
+  const appStateStore = useAppStateStore();
+
+  beforeEach(() => {
+    appStateStore.$reset();
+    scheduleStore.$reset();
+  });
+
+  it('mounts', () => {
+    const wrapper = mount(SimpleTrainingCard, {
+      props: {
+        training: basicTraining,
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('shows basic data', () => {
+    const wrapper = mount(SimpleTrainingCard, {
+      props: {
+        training: basicTraining,
+      },
+    });
+
+    expect(wrapper.find('.simple-training-card__activity-icon').attributes('aria-label')).toBe('Boxing');
+    expect(wrapper.find('.simple-training-card__title').text()).toBe('Boxing');
+    expect(wrapper.find('.simple-training-card__duration').text()).toBe('1 h');
+    expect(wrapper.find('.simple-training-card__location').text()).toBe('Total wreck gym');
+    expect(wrapper.find('.simple-training-card__intensity').text()).toBe('Medium');
+  });
+
+  it('shows custom data', async () => {
+    scheduleStore.schedule.unitOfTime = 'm';
+    const wrapper = mount(SimpleTrainingCard, {
+      props: {
+        training: {
+          ...basicTraining,
+          duration: 60,
+          location: '',
+          title: 'Free fight',
+        },
+      },
+    });
+
+    expect(wrapper.find('.simple-training-card__title').text()).toBe('Free fight');
+    expect(wrapper.find('.simple-training-card__duration').text()).toBe('60 m');
+    expect(wrapper.find('.simple-training-card__location').exists()).toBe(false);
+    expect(wrapper.find('.simple-training-card__intensity').text()).toBe('Medium');
+
+    await wrapper.setProps({
+      training: {
+        ...basicTraining,
+        duration: 0,
+      },
+    });
+
+    expect(wrapper.find('.simple-training-card__duration').exists()).toBe(false);
+  });
+});
--- src/views/PrintView.vue
+++ src/views/PrintView.vue
@@ -1,9 +1,63 @@
 <script setup lang="ts">
+import {computed} from 'vue';
 import {useI18n} from 'vue-i18n';
+import {storeToRefs} from 'pinia';
+import useWeekDays from '@/hooks/weekdays';
+import {useScheduleStore} from '@/stores/schedule';
+import SimpleTrainingCard from '@/components/SimpleTrainingCard.vue';
+import type {Week} from '@/types';
 
+const scheduleStore = useScheduleStore();
+const {schedule} = storeToRefs(scheduleStore);
 const {t} = useI18n();
+const {weekdays} = useWeekDays();
+const trainingsByDay = computed(() => (week: Week) =>
+  weekdays.value.map((weekDay, weekdayIndex) =>
+    week.trainings.filter(({dayIndex}) => dayIndex === weekdayIndex)
+  )
+);
 </script>
 
 <template>
-  <h1 class="text-h2 text-center">{{ t('print.title') }}</h1>
+  <h1 class="text-h3 text-center">{{ schedule.name || t('print.title') }}</h1>
+  <v-table v-for="(week, index) in schedule.weeks" :key="week.id" class="print-view__table">
+    <template #top>
+      <h3>{{ t('weekCalendar.weekTitle', [index + 1]) }}</h3>
+    </template>
+    <thead>
+      <tr>
+        <th v-for="day in weekdays" :key="day">{{ day }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th v-for="(trainings, index) in trainingsByDay(week)" :key="index">
+          <simple-training-card v-for="training in trainings" :key="training.id" :training="training" />
+        </th>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
+<style lang="scss" scoped>
+.simple-training-card + .simple-training-card {
+  margin-top: 1rem;
+  &:before {
+    content: '';
+    position: absolute;
+    border-top: 1px solid rgba(var(--v-border-color), var(--v-border-opacity));
+    width: 100%;
+    top: -0.5rem;
+  }
+}
+
+.print-view__table {
+  margin-top: 1rem;
+}
+
+@media print {
+  .print-view__table {
+    page-break-after: always;
+    break-after: page;
+  }
+}
+</style>
