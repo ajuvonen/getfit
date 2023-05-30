6a1b4f80c7e43c16f932f6ce6f3c2b6a9a86ce92
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-05-30 15:32:22 +0300

    Use vite preview for e2e testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,5 @@ export default defineConfig({
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:5173',
     video: false,
-    retries: {
-      runMode: 1,
-    },
   },
 });
