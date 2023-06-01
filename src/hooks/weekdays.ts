871c451312930750902f3f7d7ce27e83f7e3fa91
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-06-01 12:46:45 +0300

    Shorten weekday naming in print

--- src/hooks/weekdays.ts
+++ src/hooks/weekdays.ts
@@ -3,18 +3,23 @@ import {storeToRefs} from 'pinia';
 import {useScheduleStore} from '@/stores/schedule';
 import {useI18n} from 'vue-i18n';
 
+const sundayFirst = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+const mondayFirst = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
 export default function useWeekDays() {
   const scheduleStore = useScheduleStore();
   const {schedule} = storeToRefs(scheduleStore);
   const {t} = useI18n();
 
   const weekdays = computed(() => {
-    const dayArray = schedule.value.startsOnSunday
-      ? ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
-      : ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-
+    const dayArray = schedule.value.startsOnSunday ? sundayFirst : mondayFirst;
     return dayArray.map((day) => t(`general.weekdays.${day}`));
   });
 
-  return {weekdays};
+  const shortWeekdays = computed(() => {
+    const dayArray = schedule.value.startsOnSunday ? sundayFirst : mondayFirst;
+    return dayArray.map((day) => t(`general.shortWeekdays.${day}`));
+  });
+
+  return {weekdays, shortWeekdays};
 }
