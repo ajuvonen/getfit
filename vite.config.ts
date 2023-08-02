6725d99348e524b2209b46185f7bc37673bee54e
ajuvonen <ajuvonen@hotmail.com>
2023-08-02 08:51:06 +0300

    Return to default path resolve config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
+import {fileURLToPath, URL} from 'node:url';
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
-import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   preview: {
