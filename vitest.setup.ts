607ecf339a199d78aae52b0e688c54b95ba235b4
ajuvonen <ajuvonen@hotmail.com>
2023-12-05 17:27:21 +0200

    Update dependencies

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,5 @@
 import {vi} from 'vitest';
-import {DOMWrapper, VueWrapper, config} from '@vue/test-utils';
+import {VueWrapper, config} from '@vue/test-utils';
 import {createVuetify} from 'vuetify';
 import i18n from '@/i18n';
 import {createTestingPinia} from '@pinia/testing';
@@ -7,10 +7,7 @@ import {createTestingPinia} from '@pinia/testing';
 config.global.plugins = [createTestingPinia(), createVuetify(), i18n];
 
 const dataTestIdPlugin = (wrapper: VueWrapper) => ({
-  findByTestId: (testId: string) => {
-    const element = wrapper.element.querySelector(`[data-test-id='${testId}']`);
-    return new DOMWrapper(element);
-  },
+  findByTestId: (testId: string) => wrapper.find(`[data-test-id='${testId}']`),
 });
 
 config.plugins.VueWrapper.install(dataTestIdPlugin);
