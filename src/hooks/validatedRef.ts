63cdfcbfbbb966bb1171a303b0e3203e7be6e31f
ajuvonen <ajuvonen@hotmail.com>
2024-01-12 09:12:40 +0200

    Add validatedRef hook

---
+++ src/hooks/validatedRef.ts
@@ -0,0 +1,46 @@
+import {
+  computed,
+  watch,
+  ref,
+  type Ref,
+  type WritableComputedRef,
+  type ComputedRef,
+} from 'vue';
+import {useVuelidate, type ValidationRule} from '@vuelidate/core';
+import {getValidationErrors} from '@/utils';
+
+export default function useValidatedRef<T>(
+  original: Ref<T>,
+  key: keyof T,
+  rules: ComputedRef<{
+    [P in keyof T]?: {
+      [rule: string]: ValidationRule | undefined;
+    } | undefined;
+  }>,
+): [WritableComputedRef<T[keyof T]>, ComputedRef<string[]>] {
+  const internal = ref({...original.value}) as Ref<T>;
+  const $v = useVuelidate(rules, internal);
+  watch(
+    () => original.value[key],
+    (newValue) => {
+      if (internal.value[key] !== newValue) {
+        internal.value[key] = newValue;
+      }
+    },
+  );
+  return [
+    computed({
+      get() {
+        return internal.value[key];
+      },
+      set(value: T[typeof key]) {
+        internal.value[key] = value;
+        $v.value[key].$touch();
+        if (!$v.value[key].$error) {
+          original.value[key] = value;
+        }
+      },
+    }),
+    computed(() => getValidationErrors($v.value)),
+  ];
+}
