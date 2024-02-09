3fad691a6098bd9cfc4ff935b0e05a743f3170e8
ajuvonen <ajuvonen@hotmail.com>
2024-02-09 08:37:26 +0200

    Refactor week supplement tests

--- src/components/__tests__/WeekSupplement.spec.ts
+++ src/components/__tests__/WeekSupplement.spec.ts
@@ -1,31 +1,30 @@
 import {mount} from '@vue/test-utils';
 import {describe, it, expect} from 'vitest';
-import {v4 as uuidv4} from 'uuid';
-import {Intensity, type Training} from '@/types';
+import {getEmptyTraining} from '@/utils';
 import WeekSupplement from '@/components/WeekSupplement.vue';
-
-const trainings: Training[] = [
-  {
-    id: uuidv4(),
-    weekId: uuidv4(),
-    dayIndex: 0,
-    activity: 'boxing',
-    title: '',
-    description: 'Sparring at the gym',
-    duration: 1,
-    intensity: Intensity.MEDIUM,
-    location: 'Total wreck gym',
-  },
-];
 
 describe('WeekSupplement', () => {
   it('mounts', () => {
     const wrapper = mount(WeekSupplement, {
       props: {
-        trainings,
+        trainings: [
+          getEmptyTraining({activity: 'running'}),
+          getEmptyTraining({activity: 'swimming', description: 'swim slow'}),
+        ],
         dayIndex: 0,
       },
     });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('remains empty', () => {
+    const wrapper = mount(WeekSupplement, {
+      props: {
+        trainings: [],
+        dayIndex: 0,
+      },
+    });
+
     expect(wrapper.html()).toMatchSnapshot();
   });
 });
