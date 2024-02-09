f89c48fffd66b0b8494f6f0eb3ae85d5e97ad4df
ajuvonen <ajuvonen@hotmail.com>
2024-02-09 08:37:56 +0200

    Refactor training card style

--- src/components/TrainingCard.vue
+++ src/components/TrainingCard.vue
@@ -3,7 +3,6 @@ import {storeToRefs} from 'pinia';
 import type {Training} from '@/types';
 import {getIcon, getIntensityColor} from '@/utils';
 import {useScheduleStore} from '@/stores/schedule';
-import useScreen from '@/hooks/screen';
 import {COLORS} from '@/constants';
 import TrainingCardActions from '@/components/TrainingCardActions.vue';
 import TrainingRating from '@/components/TrainingRating.vue';
@@ -12,57 +11,52 @@ defineProps<{
   training: Training;
 }>();
 
-const {isDark} = useScreen();
-
 const {settings} = storeToRefs(useScheduleStore());
 </script>
 <template>
-  <v-card class="training-card mx-2 mt-4 mb-2" :color="isDark ? COLORS.lightGrey : undefined">
+  <v-card
+    class="training-card mx-2 mt-4 mb-2"
+    :color="getIntensityColor(training.intensity)"
+    :style="{color: COLORS.darkGrey}"
+  >
     <v-card-item class="training-card__title-wrapper">
-      <v-icon
-        :icon="getIcon(training.activity)"
-        :title="$t(`activities.${training.activity}`)"
-        :aria-label="$t(`activities.${training.activity}`)"
-        :color="isDark ? COLORS.darkGrey : COLORS.offWhite"
-        :style="{background: getIntensityColor(training.intensity)}"
-        class="training-card__activity-icon"
-        size="x-large"
-      />
-      <v-card-title class="d-flex ml-11 flex-column">
+      <div
+        class="training-card__icons d-flex align-center"
+        :class="{'training-card__icons--completed': training.completed}"
+      >
+        <v-icon
+          :icon="getIcon(training.activity)"
+          :title="
+            training.completed
+              ? $t(`trainingCard.completed`, [$t(`activities.${training.activity}`)])
+              : $t(`activities.${training.activity}`)
+          "
+          :aria-label="$t(`activities.${training.activity}`)"
+          class="training-card__activity-icon"
+          size="x-large"
+        />
+      </div>
+      <v-card-title class="ml-11">
         <div class="training-card__title">
           {{ training.title || $t(`activities.${training.activity}`) }}
         </div>
-        <div class="d-flex align-center justify-space-between">
-          <div class="d-flex flex-column">
-            <div class="training-card__duration text-subtitle-2">
-              <v-icon icon="mdi-timer" :aria-label="$t('trainingCard.duration')" />
-              {{ training.duration || '-' }} {{ settings.unitOfTime }}
-            </div>
-            <div class="training-card__intensity text-subtitle-2">
-              <v-icon icon="mdi-speedometer" :aria-label="$t('trainingCard.intensity')" />
-              {{ $t(`intensities.${training.intensity}`) }}
-            </div>
-            <div class="training-card__location text-subtitle-2">
-              <v-icon icon="mdi-map-marker" :aria-label="$t('trainingCard.location')" />
-              {{ training.location || '-' }}
-            </div>
-          </div>
-          <div class="d-flex align-center">
-            <transition name="fall">
-              <v-icon
-                v-if="training.completed"
-                size="x-large"
-                icon="mdi-check"
-                class="training-card__completion-mark"
-              />
-            </transition>
-          </div>
+        <div class="training-card__duration text-subtitle-2">
+          <v-icon icon="mdi-timer" :aria-label="$t('trainingCard.duration')" />
+          {{ training.duration || '-' }} {{ settings.unitOfTime }}
+        </div>
+        <div class="training-card__intensity text-subtitle-2">
+          <v-icon icon="mdi-speedometer" :aria-label="$t('trainingCard.intensity')" />
+          {{ $t(`intensities.${training.intensity}`) }}
+        </div>
+        <div class="training-card__location text-subtitle-2">
+          <v-icon icon="mdi-map-marker" :aria-label="$t('trainingCard.location')" />
+          {{ training.location || '-' }}
         </div>
       </v-card-title>
     </v-card-item>
     <v-card-text
-      class="training-card__text"
-      :class="training.description || training.completed ? 'pt-2 px-4' : ''"
+      class="training-card__text px-4"
+      :class="training.description || training.completed ? 'pt-2' : ''"
     >
       <span v-if="!training.completed">{{ training.description }}</span>
       <training-rating v-if="training.completed" :training="training" />
@@ -98,17 +92,23 @@ const {settings} = storeToRefs(useScheduleStore());
   white-space: pre-wrap;
 }
 
-.training-card__activity-icon {
-  border-radius: 4px;
-  border-top-left-radius: 0;
-  border-bottom-left-radius: 0;
+.training-card__icons {
   position: absolute;
   left: 0;
   top: 0;
   height: 100%;
-  padding: 0rem 1.5rem 0 3.2rem;
-  margin-left: -1.5rem;
-  box-shadow: inset -5px -5px 10px 0px rgba(33, 33, 33, 0.3);
+  padding: 0 0.7rem;
+}
+
+.training-card__icons--completed {
+  background: repeating-linear-gradient(
+    -45deg,
+    rgba(0, 0, 0, 0.2),
+    rgba(0, 0, 0, 0.2) 10px,
+    transparent 10px,
+    transparent 20px
+  );
+  border-bottom-right-radius: 4px;
 }
 
 .training-card__duration,
@@ -116,21 +116,5 @@ const {settings} = storeToRefs(useScheduleStore());
 .training-card__intensity {
   display: flex;
   gap: 0.5rem;
-}
-
-.fall-enter-from {
-  opacity: 0;
-  transform: scale(0) rotate(-20deg);
-}
-
-.fall-enter-active {
-  transition:
-    opacity 0.1s,
-    transform 0.5s ease-in;
-}
-
-.fall-enter-to {
-  opacity: 1;
-  transform: scale(1) rotate(0);
 }
 </style>
