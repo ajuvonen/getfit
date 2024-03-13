ff50b69078bb59d8e95474e1a0fe99cdc4c75e17
ajuvonen <ajuvonen@hotmail.com>
2024-03-13 11:13:22 +0200

    Add InfoDialog component

---
+++ src/components/InfoDialog.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import BaseDialog from '@/components/BaseDialog.vue';
+
+defineProps<{
+  open: boolean;
+  title: string;
+}>();
+
+defineEmits(['close']);
+</script>
+<template>
+  <BaseDialog :open="open" :use-full-screen="false" :title="title">
+    <template #content>
+      <slot name="content" />
+    </template>
+    <template #actions>
+      <v-btn
+        color="primary"
+        prepend-icon="mdi-close"
+        data-test-id="info-dialog-close-button"
+        @click="$emit('close')"
+        >{{ $t('general.close') }}</v-btn
+      >
+    </template>
+  </BaseDialog>
+</template>
