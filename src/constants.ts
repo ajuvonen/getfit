2e5e2a3e484914f7ae3cc0f771307fd5cad17f26
ajuvonen <ajuvonen@hotmail.com>
2024-03-13 11:13:01 +0200

    Alter intensity colors and add photo credits

--- src/constants.ts
+++ src/constants.ts
@@ -96,10 +96,28 @@ export const WEEKDAYS = {
 
 export const COLORS = {
   darkGrey: '#212121',
-  intensityLight: '#6DBF79',
-  intensityNormal: '#FFC300',
-  intensityDemanding: '#FF8C00',
-  intensityHeavy: '#FF6347',
+  intensityLight: '#9ED99D',
+  intensityNormal: '#FFD98C',
+  intensityDemanding: '#FFB366',
+  intensityHeavy: '#FF7F7A',
   lightGrey: '#3C3C3C',
   offWhite: '#FAFAFA',
 };
+
+export const PHOTO_CREDITS = [
+  {name: 'Alexa Kozik', link: 'https://www.pexels.com/@alesiakozik/'},
+  {name: 'Leo Zhao', link: 'https://www.pexels.com/@leozhao/'},
+  {name: 'Andrea Piacquadio', link: 'https://www.pexels.com/@olly/'},
+  {name: 'Chris Peeters', link: 'https://www.pexels.com/@krizz59/'},
+  {name: 'cottonbro studio', link: 'https://www.pexels.com/@cottonbro/'},
+  {name: 'Fede Roveda', link: 'https://www.pexels.com/@fede-roveda-1461538/'},
+  {name: 'Flo Maderebner', link: 'https://www.pexels.com/@fmaderebner/'},
+  {name: 'JD Manning', link: 'https://www.pexels.com/@jddaniel/'},
+  {name: 'Mali Maeder', link: 'https://www.pexels.com/@mali/'},
+  {name: 'Mikhail Nilov', link: 'https://www.pexels.com/@mikhail-nilov/'},
+  {name: 'Oleksandr P', link: 'https://www.pexels.com/@freestockpro/'},
+  {name: 'Pavel Danilyuk', link: 'https://www.pexels.com/@pavel-danilyuk/'},
+  {name: 'Pixabay', link: 'https://www.pexels.com/@pixabay/'},
+  {name: 'RDNE Stock Project', link: 'https://www.pexels.com/@rdne/'},
+  {name: 'Victor Freitas', link: 'https://www.pexels.com/@victorfreitas/'},
+];
