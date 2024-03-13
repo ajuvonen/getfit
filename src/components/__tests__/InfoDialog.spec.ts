2044cc15898d3a88d66f84843cf1753505c64b0b
ajuvonen <ajuvonen@hotmail.com>
2024-03-13 11:13:22 +0200

    Add InfoDialog component

---
+++ src/components/__tests__/InfoDialog.spec.ts
@@ -0,0 +1,30 @@
+import {describe, it, expect} from 'vitest';
+import {mount} from '@vue/test-utils';
+import {VCard} from 'vuetify/components';
+import InfoDialog from '@/components/InfoDialog.vue';
+
+describe('InfoDialog', () => {
+  it('mounts', () => {
+    const wrapper = mount(InfoDialog, {
+      props: {
+        open: true,
+        title: 'Info',
+      },
+      slots: {
+        content: 'My content',
+      },
+    });
+    expect(wrapper.findComponent(VCard).html()).toMatchSnapshot();
+  });
+
+  it('close works', async () => {
+    const wrapper = mount(InfoDialog, {
+      props: {
+        open: true,
+        title: 'Info',
+      },
+    });
+    await wrapper.findComponent(VCard).findByTestId('info-dialog-close-button').trigger('click');
+    expect(wrapper.emitted().close.length).toEqual(1);
+  });
+});
