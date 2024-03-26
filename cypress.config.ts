07d31fd897ea5417a09b81a52b244cc117231880
ajuvonen <ajuvonen@hotmail.com>
2024-03-26 15:29:52 +0200

    Decrease e2e page load timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,9 @@ import {defineConfig} from 'cypress';
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.ts',
-    baseUrl: 'http://localhost:5173/getfit/',
+    baseUrl: 'http://localhost:5173/getfit',
     video: false,
     trashAssetsBeforeRuns: true,
+    pageLoadTimeout: 5000,
   },
 });
