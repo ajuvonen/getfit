341b168839b08185dac7fab0f5e840a449beb017
ajuvonen <ajuvonen@hotmail.com>
2024-03-26 15:11:24 +0200

    Make GetFit installable

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,41 @@
 import {fileURLToPath, URL} from 'node:url';
 import {defineConfig} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify()],
+  plugins: [
+    vue(),
+    vuetify(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'GetFit Training Schedule Planner',
+        short_name: 'GetFit',
+        description: 'GetFit is a free app for planning and tracking your training schedule.',
+        dir: 'ltr',
+        icons: [
+          {
+            src: 'pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: ['any', 'maskable'],
+          },
+        ],
+        theme_color: '#FAFAFA',
+        background_color: '#FAFAFA',
+        categories: ['fitness', 'sports', 'health'],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,jpg,woff,woff2,eot,ttf}'],
+      },
+      devOptions: {
+        enabled: process.env.NODE_ENV === 'development',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -14,5 +44,5 @@ export default defineConfig({
   preview: {
     port: 5173,
   },
-  base: '/getfit/',
+  base: '/getfit',
 });
