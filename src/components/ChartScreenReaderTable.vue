0dad1a5b1f74c4a086524c7104f41e66e03760f8
ajuvonen <ajuvonen@hotmail.com>
2024-05-07 16:15:02 +0300

    Add stats view and basic charts

---
+++ src/components/ChartScreenReaderTable.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+defineProps<{
+  id: string,
+  title: string,
+  labels: string[],
+  data: number[],
+}>();
+</script>
+<template>
+  <table class="d-sr-only" :id="id">
+    <caption>{{ title }}</caption>
+    <thead>
+      <tr>
+        <th v-for="label in labels" :key="label">{{ label }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td v-for="count in data" :key="count">{{ count }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ src/components/IntensityChart.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+import {computed} from 'vue';
+import {storeToRefs} from 'pinia';
+import {useI18n} from 'vue-i18n';
+import {Chart as ChartJS, ArcElement, Tooltip, Legend, Title} from 'chart.js';
+import {Pie} from 'vue-chartjs';
+import {groupBy, prop} from 'remeda';
+import {useScheduleStore} from '@/stores/schedule';
+import {COLORS} from '@/constants';
+import {Intensity} from '@/types';
+import {getChartTitleOptions} from '@/utils';
+import useScreen from '@/hooks/screen';
+import ChartScreenReaderTable from '@/components/ChartScreenReaderTable.vue';
+
+ChartJS.register(ArcElement, Legend, Tooltip, Title);
+
+const {t} = useI18n();
+
+const scheduleStore = useScheduleStore();
+const {getAllTrainings} = storeToRefs(scheduleStore);
+
+const {isDark} = useScreen();
+
+const chartData = computed(() => {
+  const trainings = groupBy(getAllTrainings.value, prop('intensity'));
+  const colors: string[] = [];
+  if (trainings[Intensity.LIGHT]) colors.push(COLORS.intensityLight);
+  if (trainings[Intensity.NORMAL]) colors.push(COLORS.intensityNormal);
+  if (trainings[Intensity.DEMANDING]) colors.push(COLORS.intensityDemanding);
+  if (trainings[Intensity.HEAVY]) colors.push(COLORS.intensityHeavy);
+  return {
+    labels: Object.keys(trainings).map((key) => t(`intensities.${key}`)),
+    datasets: [
+      {
+        data: Object.values(trainings).map(({length}) => length),
+        backgroundColor: colors,
+      },
+    ],
+  };
+});
+
+const chartOptions = computed(() => ({
+  responsive: true,
+  ...getChartTitleOptions<'pie'>(t('stats.trainingsByIntensity'), isDark.value),
+}));
+</script>
+<template>
+  <Pie :options="chartOptions" :data="chartData" aria-describedby="intensity-chart-table" />
+  <ChartScreenReaderTable
+    id="intensity-chart-table"
+    :title="t('stats.trainingsByIntensity')"
+    :labels="chartData.labels"
+    :data="chartData.datasets[0].data"
+  />
+</template>
