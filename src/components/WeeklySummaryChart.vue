59018b5104842d0d72aad864ef64d6355facaf25
ajuvonen <ajuvonen@hotmail.com>
2024-05-08 14:34:25 +0300

    Add weekly charts

---
+++ src/components/WeeklySummaryChart.vue
@@ -0,0 +1,86 @@
+<script lang="ts" setup>
+import {computed} from 'vue';
+import {storeToRefs} from 'pinia';
+import {useI18n} from 'vue-i18n';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import {Bar} from 'vue-chartjs';
+import {prop, sumBy} from 'remeda';
+import {useScheduleStore} from '@/stores/schedule';
+import {getChartTitleOptions} from '@/utils';
+import useScreen from '@/hooks/screen';
+import useWeekDays from '@/hooks/weekdays';
+import {COLORS} from '@/constants';
+import ChartScreenReaderTable from '@/components/ChartScreenReaderTable.vue';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+const {t} = useI18n();
+
+const scheduleStore = useScheduleStore();
+const {weeks} = storeToRefs(scheduleStore);
+
+const {isDark} = useScreen();
+
+const {getDisplayWeekNumber} = useWeekDays();
+
+const chartData = computed(() => {
+  const labels: string[] = [];
+  const datasets = [
+    {
+      label: t('general.unitsOfDuration.h'),
+      data: [] as number[],
+      backgroundColor: COLORS.chartColors[0],
+    },
+    {
+      label: t('general.unitsOfDuration.km'),
+      data: [] as number[],
+      backgroundColor: COLORS.chartColors[3],
+    },
+    {
+      label: t('general.unitsOfDuration.mi'),
+      data: [] as number[],
+      backgroundColor: COLORS.chartColors[5],
+    },
+  ];
+
+  weeks.value.forEach(({trainings}, index) => {
+    labels.push(t('weekCalendar.weekTitle', [getDisplayWeekNumber.value(index)]));
+    const hours = sumBy(trainings, ({unitOfDuration, duration}) => unitOfDuration === 'h' ? duration : 0);
+    const minutes = sumBy(trainings, ({unitOfDuration, duration}) => unitOfDuration === 'm' ? duration : 0);
+    const kilometers = sumBy(trainings, ({unitOfDuration, duration}) => unitOfDuration === 'km' ? duration : 0);
+    const miles = sumBy(trainings, ({unitOfDuration, duration}) => unitOfDuration === 'mi' ? duration : 0);
+    datasets[0].data.push(Math.round((hours + minutes / 60) * 4) / 4);
+    datasets[1].data.push(kilometers);
+    datasets[2].data.push(miles);
+  });
+
+  return {
+    labels,
+    datasets: datasets.filter(({data}) => data.some((value) => value)),
+  };
+});
+
+const chartOptions = computed(() => ({
+  responsive: true,
+  maintainAspectRatio: false,
+  ...getChartTitleOptions<'bar'>(t('stats.weeklySummary'), isDark.value),
+}));
+</script>
+<template>
+  <Bar :options="chartOptions" :data="chartData" aria-describedby="weekly-summary-chart-table" />
+  <ChartScreenReaderTable
+    id="weekly-summary-chart-table"
+    :title="$t('stats.weeklySummary')"
+    :columnHeaders="chartData.labels"
+    :rowHeaders="chartData.datasets.map(prop('label'))"
+    :data="chartData.datasets.map(prop('data'))"
+  />
+</template>
--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -3,6 +3,8 @@ import BaseView from '@/components/BaseView.vue';
 import ActivityChart from '@/components/ActivityChart.vue';
 import IntensityChart from '@/components/IntensityChart.vue';
 import RatingChart from '@/components/RatingChart.vue';
+import WeeklyTrainingsChart from '@/components/WeeklyTrainingsChart.vue';
+import WeeklySummaryChart from '@/components/WeeklySummaryChart.vue';
 </script>
 <template>
   <BaseView :title="$t('routes.stats')" :guide="$t('stats.guide')">
@@ -18,6 +20,16 @@ import RatingChart from '@/components/RatingChart.vue';
           <RatingChart />
         </v-col>
       </v-row>
+      <v-row>
+        <v-col class="stats__wide-chart">
+          <WeeklyTrainingsChart />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="stats__wide-chart">
+          <WeeklySummaryChart />
+        </v-col>
+      </v-row>
     </template>
   </BaseView>
 </template>
@@ -25,5 +37,9 @@ import RatingChart from '@/components/RatingChart.vue';
 .stats__chart {
   max-width: 400px;
   max-height: 400px;
+}
+
+.stats__wide-chart {
+  min-height: 300px;
 }
 </style>
