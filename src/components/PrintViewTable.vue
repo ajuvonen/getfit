a420aeaf12f9e83e624fef8ea3543d5aefd18a5b
ajuvonen <ajuvonen@hotmail.com>
2024-05-14 08:19:48 +0300

    Remove stripes from supplement printouts

--- src/components/PrintViewTable.vue
+++ src/components/PrintViewTable.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
-defineProps<{
-  tableTitle?: string;
-}>();
+withDefaults(
+  defineProps<{
+    tableTitle?: string;
+    striped?: boolean;
+  }>(),
+  {
+    tableTitle: '',
+    striped: true,
+  },
+);
 </script>
 <template>
-  <v-table class="print-view__table bg-transparent">
+  <v-table
+    class="print-view__table bg-transparent"
+    :class="{'print-view__table--striped': striped}"
+  >
     <template #top>
       <slot name="title"></slot>
       <h2 v-if="!$slots.title" class="text-h5 ml-4">{{ tableTitle }}</h2>
@@ -29,12 +39,14 @@ defineProps<{
 }
 
 @media print {
-  :deep(td:nth-child(even)), :deep(th:nth-child(even)) {
-    background-color: #FAFAFA;
-    print-color-adjust: exact !important;
+  .print-view__table--striped {
+    :deep(td:nth-child(even)),
+    :deep(th:nth-child(even)) {
+      background-color: #fafafa;
+      print-color-adjust: exact !important;
+    }
   }
   .print-view__table {
-    display: block;
     page-break-after: always;
     break-after: page;
   }
