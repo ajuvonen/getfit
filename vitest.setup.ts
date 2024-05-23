45f2ac54ba695333d46c8d6eb70a29f753cf58cd
ajuvonen <ajuvonen@hotmail.com>
2024-05-23 15:18:11 +0300

    Do pinia init in same hook as other setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,12 +1,12 @@
 import {DOMWrapper, VueWrapper, config} from '@vue/test-utils';
-import {afterEach, beforeEach, vi} from 'vitest';
+import {beforeEach, vi} from 'vitest';
 import {createTestingPinia} from '@pinia/testing';
 import resizeObserver from 'resize-observer-polyfill';
 import i18n from '@/i18n';
 import router from '@/router';
 import vuetifyPlugin from '@/vuetify';
 
-config.global.plugins = [createTestingPinia(), vuetifyPlugin, i18n, router];
+config.global.plugins = [vuetifyPlugin, i18n, router];
 vi.stubGlobal('ResizeObserver', resizeObserver);
 
 const dataTestIdPlugin = (wrapper: VueWrapper) => ({
@@ -19,10 +19,7 @@ const dataTestIdPlugin = (wrapper: VueWrapper) => ({
 beforeEach(() => {
   // Clear local storage so state is fresh for each test
   localStorage.clear();
-});
-
-afterEach(() => {
-  config.global.plugins[0] = createTestingPinia();
+  config.global.plugins[3] = createTestingPinia();
 });
 
 config.plugins.VueWrapper.install(dataTestIdPlugin);
