b1f3d451d2007e23c25398f5485d8bb9006f5337
ajuvonen <ajuvonen@hotmail.com>
2024-05-28 13:06:25 +0300

    Use typed localization keys

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,11 @@
 import {createI18n} from 'vue-i18n';
+import type {LocalizationKey} from '@/types';
 import en from './en.json';
 import fi from './fi.json';
 
 type MessageSchema = typeof en;
 
-export default createI18n<[MessageSchema], 'en' | 'fi'>({
+export default createI18n<[MessageSchema], LocalizationKey>({
   locale: 'en',
   fallbackLocale: 'en',
   legacy: false,
